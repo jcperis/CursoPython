401e8faaf96e2317c7e32672d4651ba2f28f6d43
Juan Carlos <perisjc@gmail.com>
2025-07-31 16:52:48 +0200

    url en accesoWeb

--- Clase6_Pandas/accesoWebAPIs.py
+++ Clase6_Pandas/accesoWebAPIs.py
@@ -9,10 +9,11 @@ url = f"{url_ref_cat}{ref_cat}"
 
 # Intentar realizar la petición a la API
 try:
-    response = requests.get(url_ref_cat)
+    response = requests.get(url)
     response.raise_for_status()
     # Imprimir la respuesta en formato JSON.
     # Aquí podriamos manejar los datos obtenidos a nuestra conveniencia.
-    print(response.json())
+    datos = response.json()
+    print(datos)
 except Exception as e:
     print(f"Error al acceder a la API: {e}")
