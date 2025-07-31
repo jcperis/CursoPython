f8ce1a8520c7c524db0349d00d14303ce73c6516
Juan Carlos <perisjc@gmail.com>
2025-07-31 16:41:50 +0200

    Clase7

---
+++ Clase6_Pandas/accesoWebAPIs.py
@@ -0,0 +1,18 @@
+import requests
+
+# Crear una URL para obtener los datos
+# Datos de una vivienda
+url_ref_cat = "https://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero/COVCCallejero.svc/json/Consulta_DNPRC?RefCat="
+ref_cat = input("Introduce la referencia catastral: ")
+url = f"{url_ref_cat}{ref_cat}"
+
+
+# Intentar realizar la petición a la API
+try:
+    response = requests.get(url_ref_cat)
+    response.raise_for_status()
+    # Imprimir la respuesta en formato JSON.
+    # Aquí podriamos manejar los datos obtenidos a nuestra conveniencia.
+    print(response.json())
+except Exception as e:
+    print(f"Error al acceder a la API: {e}")
